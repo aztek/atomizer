61569ab96f127fc340208a88f5eae7da0ee9c52b
aztek <evgeny.kotelnikov@gmail.com>
2019-10-14 00:08:14 +0200

    Switch to 4 space indents

--- src/atomizer.erl
+++ src/atomizer.erl
@@ -17,91 +17,91 @@
 
 -spec atomize(source()) -> {ok, bags:bag(atom(), location())} | {error, term()}.
 atomize(Source) ->
-  Pid = self(),
-  Callback = spawn_link(fun () -> init(Pid) end),
-  ok = collect_files(Callback, Source),
-  Callback ! done_collecting_files,
-  receive
-    Msg -> Msg
-  end.
+    Pid = self(),
+    Callback = spawn_link(fun () -> init(Pid) end),
+    ok = collect_files(Callback, Source),
+    Callback ! done_collecting_files,
+    receive
+        Msg -> Msg
+    end.
 
 -record(state, {
-  expect_files = true         :: boolean(),
-  file_names   = sets:new()   :: sets:set(file:filename()),
-  atoms        = bags:empty() :: bags:bag(atom(), location())
+    expect_files = true         :: boolean(),
+    file_names   = sets:new()   :: sets:set(file:filename()),
+    atoms        = bags:empty() :: bags:bag(atom(), location())
 }).
 
 -spec init(pid()) -> ok.
 init(Callback) ->
-  put(callback, Callback),
-  loop(#state{}).
+    put(callback, Callback),
+    loop(#state{}).
 
 -spec loop(#state{}) -> ok.
 loop(State) ->
-  #state{
-    expect_files = ExpectFiles,
-    file_names   = FileNames,
-    atoms        = Atoms
-  } = State,
-  case sets:is_empty(FileNames) and (ExpectFiles == false) of
-    true ->
-      get(callback) ! {ok, Atoms},
-      ok;
-    false ->
-      receive
-        {file, FileName} ->
-          spawn_link(atomizer_parse, parse_file, [self(), FileName]),
-          loop(State#state{file_names = sets:add_element(FileName, FileNames)});
+    #state{
+        expect_files = ExpectFiles,
+        file_names   = FileNames,
+        atoms        = Atoms
+    } = State,
+    case sets:is_empty(FileNames) and (ExpectFiles == false) of
+        true ->
+            get(callback) ! {ok, Atoms},
+            ok;
+        false ->
+            receive
+                {file, FileName} ->
+                    spawn_link(atomizer_parse, parse_file, [self(), FileName]),
+                    loop(State#state{file_names = sets:add_element(FileName, FileNames)});
 
-        {atom, Atom, FileName, Location} ->
-          loop(State#state{atoms = bags:put(Atom, {FileName, Location}, Atoms)});
+                {atom, Atom, FileName, Location} ->
+                    loop(State#state{atoms = bags:put(Atom, {FileName, Location}, Atoms)});
 
-        {done_parsing_file, FileName} ->
-          loop(State#state{file_names = sets:del_element(FileName, FileNames)});
+                {done_parsing_file, FileName} ->
+                    loop(State#state{file_names = sets:del_element(FileName, FileNames)});
 
-        done_collecting_files ->
-          loop(State#state{expect_files = false});
+                done_collecting_files ->
+                    loop(State#state{expect_files = false});
 
-        {error, Error} ->
-          get(callback) ! {error, Error},
-          ok
-      end
-  end.
+                {error, Error} ->
+                    get(callback) ! {error, Error},
+                    ok
+            end
+    end.
 
 -spec collect_files(Callback :: pid(), source()) -> ok.
 collect_files(Callback, {file, File}) ->
-  Callback ! {file, File},
-  ok;
+    Callback ! {file, File},
+    ok;
 
 collect_files(Callback, {dir, Dir}) ->
-  {Files, Dirs} = list_dir(Dir),
-  collect_files(Callback, {files, Files}),
-  collect_files(Callback, {dirs, Dirs});
+    {Files, Dirs} = list_dir(Dir),
+    collect_files(Callback, {files, Files}),
+    collect_files(Callback, {dirs, Dirs});
 
 collect_files(Callback, {files, Files}) ->
-  lists:foreach(fun (File) -> collect_files(Callback, {file, File}) end, Files),
-  ok;
+    lists:foreach(fun (File) -> collect_files(Callback, {file, File}) end, Files),
+    ok;
 
 collect_files(Callback, {dirs, Dirs}) ->
-  lists:foreach(fun (Dir) -> collect_files(Callback, {dir, Dir}) end, Dirs),
-  ok.
+    lists:foreach(fun (Dir) -> collect_files(Callback, {dir, Dir}) end, Dirs),
+    ok.
 
 -spec list_dir(Dir) -> {Files :: [file:filename()], Dirs :: [file:filename()]}.
 list_dir(Dir) ->
-  {ok, Names} = file:list_dir(Dir),
-  lists:foldl(fun (Name, {Files, Dirs}) ->
-                Path = Dir ++ "/" ++ Name,
-                {ok, Info} = file:read_file_info(Path),
-                case Info#file_info.type of
-                  directory -> {Files, [Path | Dirs]};
-                  regular ->
-                    case is_erlang(Name) of
-                      true  -> {[Path | Files], Dirs};
-                      false -> {Files, Dirs}
+    {ok, Names} = file:list_dir(Dir),
+    lists:foldl(fun (Name, {Files, Dirs}) ->
+                    Path = Dir ++ "/" ++ Name,
+                    {ok, Info} = file:read_file_info(Path),
+                    case Info#file_info.type of
+                        directory -> {Files, [Path | Dirs]};
+                        regular ->
+                            case is_erlang(Name) of
+                                true  -> {[Path | Files], Dirs};
+                                false -> {Files, Dirs}
+                            end
                     end
-                end
-              end, {[], []}, Names).
+                end, {[], []}, Names).
 
 -spec is_erlang(file:filename()) -> boolean().
 is_erlang(FileName) ->
-  lists:any(fun (Ext) -> lists:suffix(Ext, FileName) end, ?ERLANG_EXTENSIONS).
+    lists:any(fun (Ext) -> lists:suffix(Ext, FileName) end, ?ERLANG_EXTENSIONS).
--- src/atomizer_cli.erl
+++ src/atomizer_cli.erl
@@ -10,43 +10,43 @@
 
 -spec main([string()]) -> no_return().
 main(CmdArgs) ->
-  show(CmdArgs).
+    show(CmdArgs).
 
 list_files(Files) -> list({files, Files}).
 list_dirs(Dirs)   -> list({dirs, Dirs}).
 
 list(Source) ->
-  case atomizer:atomize(Source) of
-    {ok, Atoms}    -> list_atoms(Atoms);
-    {error, Error} -> io:format(standard_error, "Error: ~s~n", [Error])
-  end.
+    case atomizer:atomize(Source) of
+        {ok, Atoms}    -> list_atoms(Atoms);
+        {error, Error} -> io:format(standard_error, "Error: ~s~n", [Error])
+    end.
 
 show_files(Files) -> show({files, Files}).
 show_dirs(Dirs)   -> show({dirs, Dirs}).
 
 show(Source) ->
-  case atomizer:atomize(Source) of
-    {ok, Atoms}    -> show_atoms(Atoms);
-    {error, Error} -> io:format(standard_error, "Error: ~s~n", [Error])
-  end.
+    case atomizer:atomize(Source) of
+        {ok, Atoms}    -> show_atoms(Atoms);
+        {error, Error} -> io:format(standard_error, "Error: ~s~n", [Error])
+    end.
 
 -spec list_atoms(bags:bag(atom(), atomizer:location())) -> ok.
 list_atoms(Atoms) ->
-  case lists:sort(maps:keys(Atoms)) of
-    []   -> io:format("No atoms~n", []);
-    Keys -> lists:foreach(fun (Atom) -> io:format("~p~n", [Atom]) end, Keys)
-  end.
+    case lists:sort(maps:keys(Atoms)) of
+        []   -> io:format("No atoms~n", []);
+        Keys -> lists:foreach(fun (Atom) -> io:format("~p~n", [Atom]) end, Keys)
+    end.
 
 -spec show_atoms(bags:bag(atom(), atomizer:location())) -> ok.
 show_atoms(Atoms) ->
-  lists:foreach(fun (Atom) -> show_atom(Atom, maps:get(Atom, Atoms)) end,
-                lists:sort(maps:keys(Atoms))).
+    lists:foreach(fun (Atom) -> show_atom(Atom, maps:get(Atom, Atoms)) end,
+                  lists:sort(maps:keys(Atoms))).
 
 -spec show_atom(atom(), sets:set(atomizer:location())) -> ok.
 show_atom(Atom, Locations) ->
-  io:format("~p~n", [Atom]),
-  lists:foreach(fun show_location/1, lists:sort(sets:to_list(Locations))),
-  io:format("~n").
+    io:format("~p~n", [Atom]),
+    lists:foreach(fun show_location/1, lists:sort(sets:to_list(Locations))),
+    io:format("~n").
 
 -spec show_location(atomizer:location()) -> ok.
 show_location({F, L}) -> io:format("~s:~p~n", [filename:absname(F), L]).
