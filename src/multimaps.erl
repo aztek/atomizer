233424273636646c3d63477d026db314e4865576
Evgenii Kotelnikov <aztek@users.noreply.github.com>
2019-11-19 18:00:50 +0100

    Remove unused function

--- src/multimaps.erl
+++ src/multimaps.erl
@@ -4,7 +4,7 @@
 
 -opaque multimap(Key, Value) :: #{Key => sets:set(Value)}.
 
--export([empty/0, from_list/1, put/3, append_bags/2]).
+-export([empty/0, from_list/1, put/3]).
 
 -spec empty() -> multimap(Key :: term(), Value :: term()).
 empty() -> #{}.
@@ -21,11 +21,3 @@ from_list(List) ->
 put(Key, Value, Multimap) ->
     Set = maps:get(Key, Multimap, sets:new()),
     maps:put(Key, sets:add_element(Value, Set), Multimap).
-
--spec append_bags(multimap(Key, Value), multimap(Key, Value)) -> multimap(Key, Value) when
-    Key   :: term(),
-    Value :: term().
-append_bags(Multimap1, Multimap2) ->
-    maps:fold(fun (Key, Set, Multimap) ->
-        maps:update_with(Key, fun (Set1) -> sets:union(Set, Set1) end, Set, Multimap)
-    end, Multimap2, Multimap1).
