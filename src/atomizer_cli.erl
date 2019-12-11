60c11484bf591c38aa2b74a4182fd8fd100d91df
aztek <evgeny.kotelnikov@gmail.com>
2019-12-11 21:37:46 +0100

    Add the Verbose parameter

--- src/atomizer_cli.erl
+++ src/atomizer_cli.erl
@@ -4,15 +4,19 @@
     main/1,
     list_files/1,
     show_files/1,
+    show_files/2,
     warn_files/1,
-    show_dirs/1,
+    warn_files/2,
     list_dirs/1,
-    warn_dirs/1
+    show_dirs/1,
+    show_dirs/2,
+    warn_dirs/1,
+    warn_dirs/2
 ]).
 
 -spec main([string()]) -> no_return().
 main(CmdArgs) ->
-    show(CmdArgs).
+    show(CmdArgs, true).
 
 list_files(Files) -> list({files, Files}).
 list_dirs(Dirs)   -> list({dirs, Dirs}).
@@ -30,49 +34,69 @@ list_atoms(Atoms) ->
         Keys -> lists:foreach(fun (Atom) -> io:format("~p~n", [Atom]) end, Keys)
     end.
 
-show_files(Files) -> show({files, Files}).
-show_dirs(Dirs)   -> show({dirs, Dirs}).
+show_files(Files) -> show_files(Files, false).
+show_files(Files, Verbose) -> show({files, Files}, Verbose).
 
-show(Source) ->
+show_dirs(Dirs) -> show_dirs(Dirs, false).
+show_dirs(Dirs, Verbose)  -> show({dirs, Dirs}, Verbose).
+
+show(Source, Verbose) ->
     case atomizer:atomize(Source) of
-        {ok, Atoms, _} -> show_atoms(Atoms);
+        {ok, Atoms, _} -> show_atoms(Atoms, Verbose);
         {error, Error} -> io:format(standard_error, "Error: ~p~n", [Error])
     end.
 
--spec show_atoms(atomizer:atoms()) -> ok.
-show_atoms(Atoms) ->
-    lists:foreach(fun (Atom) -> show_atom(Atom, maps:get(Atom, Atoms)) end,
+-spec show_atoms(atomizer:atoms(), boolean()) -> ok.
+show_atoms(Atoms, Verbose) ->
+    lists:foreach(fun (Atom) -> show_atom(Atom, maps:get(Atom, Atoms), Verbose) end,
                   lists:sort(maps:keys(Atoms))).
 
--spec show_atom(atom(), atomizer:locations()) -> ok.
-show_atom(Atom, Locations) ->
+-spec show_atom(atom(), atomizer:locations(), boolean()) -> ok.
+show_atom(Atom, Locations, Verbose) ->
     io:format("~p~n", [Atom]),
-    lists:foreach(fun (File) -> show_location(File, maps:get(File, Locations)) end,
-                  lists:sort(maps:keys(Locations))),
+    Info = [{filename:absname(File), Positions, sets:size(Positions)} ||
+            {File, Positions} <- maps:to_list(Locations)],
+    lists:foreach(fun ({File, Positions, NrPositions}) ->
+                      case Verbose of
+                          true  -> show_location(File, Positions);
+                          false -> show_location(File, NrPositions)
+                      end
+                  end,
+                  lists:keysort(3, Info)),
     io:format("~n").
 
--spec show_location(file:filename(), atomizer:positions()) -> ok.
+-spec show_location(file:filename(), non_neg_integer() | atomizer:positions()) -> ok.
+show_location(Filename, NrPositions) when is_integer(NrPositions) ->
+    Occurrences = case NrPositions of
+                      1 -> "occurrence";
+                      _ -> "occurrences"
+                  end,
+    io:format("~s (~w ~s)~n", [Filename, NrPositions, Occurrences]);
+
 show_location(Filename, Positions) ->
-    lists:foreach(fun (Position) -> io:format("~s:~p~n", [filename:absname(Filename), Position]) end,
+    lists:foreach(fun (Position) -> io:format("~s:~w~n", [Filename, Position]) end,
                   lists:sort(sets:to_list(Positions))).
 
-warn_files(Files) -> warn({files, Files}).
-warn_dirs(Dirs)   -> warn({dirs, Dirs}).
+warn_files(Files) -> warn_files(Files, false).
+warn_files(Files, Verbose) -> warn({files, Files}, Verbose).
 
-warn(Source) ->
+warn_dirs(Dirs) -> warn_dirs(Dirs, false).
+warn_dirs(Dirs, Verbose) -> warn({dirs, Dirs}, Verbose).
+
+warn(Source, Verbose) ->
     case atomizer:atomize(Source) of
         {ok, Atoms, Warnings} ->
             NrWarnings = length(sets:to_list(Warnings)),
             NrAtoms = length(maps:keys(Atoms)),
             io:format("Found ~p warning(s) in ~p atoms(s)~n", [NrWarnings, NrAtoms]),
-            lists:foreach(fun (Warning) -> warn(Atoms, Warning) end,
+            lists:foreach(fun (Warning) -> warn(Atoms, Warning, Verbose) end,
                           sets:to_list(Warnings));
 
         {error, Error} ->
             io:format(standard_error, "Error: ~p~n", [Error])
     end.
 
-warn(Atoms, {A, B}) ->
+warn(Atoms, {A, B}, Verbose) ->
     io:format("Possible typo: ~w vs ~w~n~n", [A, B]),
-    show_atom(A, maps:get(A, Atoms)),
-    show_atom(B, maps:get(B, Atoms)).
+    show_atom(A, maps:get(A, Atoms), Verbose),
+    show_atom(B, maps:get(B, Atoms), Verbose).
