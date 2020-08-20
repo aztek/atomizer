994454f1ff9229ac53651566d474ce1767e8f2da
Evgenii Kotelnikov <evgeny.kotelnikov@gmail.com>
2020-08-20 20:06:11 +0200

    Make atomizer_compare a gen_server

--- src/atomizer_compare.erl
+++ src/atomizer_compare.erl
@@ -1,59 +1,61 @@
 -module(atomizer_compare).
 
+-behavior(gen_server).
+
 -export([
     start/0,
     atom/1,
-    stop/0
+    stop/0,
+
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    terminate/2
 ]).
 
--define(PROCESS_NAME, ?MODULE).
-
--spec start() -> true.
 start() ->
-    register(?PROCESS_NAME, spawn_link(fun () -> compare() end)).
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
 
--spec atom(atom()) -> ok.
 atom(Atom) ->
-    ?PROCESS_NAME ! {atom, Atom},
-    ok.
+    gen_server:cast(?MODULE, {atom, Atom}).
 
--spec stop() -> ok.
 stop() ->
-    ?PROCESS_NAME ! done_atoms,
-    ok.
+    gen_server:cast(?MODULE, done_atoms).
 
--spec compare() -> ok.
-compare() ->
+
+init(_Args) ->
     Atoms = ets:new(atoms, [private, set]),
-    NFs = ets:new(nfs, [private, bag]),
-    loop(Atoms, NFs),
-    ets:delete(NFs),
-    ets:delete(Atoms),
-    ok.
+    NFs   = ets:new(nfs,   [private, bag]),
+    {ok, {Atoms, NFs}}.
 
--spec loop(ets:tid(), ets:tid()) -> ok.
-loop(Atoms, NFs) ->
-    receive
-        {atom, Atom} ->
-            case insignificant(Atom) orelse ets:member(Atoms, Atom) of
-                true -> ignore;
-                false ->
-                    ets:insert(Atoms, {Atom}),
-                    lists:foreach(fun (NormalForm) ->
-                                      case ets:lookup(NFs, NormalForm) of
-                                          [{_, Lookalike}] ->
-                                              atomizer_sup:lookalikes(Atom, Lookalike);
-                                          [] ->
-                                              ets:insert(NFs, {NormalForm, Atom})
-                                      end
-                                  end,
-                                  atomizer_normalize:normalize(Atom))
-            end,
-            loop(Atoms, NFs);
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
 
-        done_atoms ->
-            atomizer_sup:stop()
-    end.
+handle_cast({atom, Atom}, State = {Atoms, NFs}) ->
+    case insignificant(Atom) orelse ets:member(Atoms, Atom) of
+        true -> ignore;
+        false ->
+            ets:insert(Atoms, {Atom}),
+            lists:foreach(fun (NormalForm) ->
+                              case ets:lookup(NFs, NormalForm) of
+                                  [{_, Lookalike}] ->
+                                      atomizer_sup:lookalikes(Atom, Lookalike);
+                                  [] ->
+                                      ets:insert(NFs, {NormalForm, Atom})
+                              end
+                          end,
+                atomizer_normalize:normalize(Atom))
+    end,
+    {noreply, State};
+
+handle_cast(done_atoms, State) ->
+    atomizer_sup:stop(),
+    {noreply, State}.
 
 insignificant(Atom) ->
     length(atom_to_list(Atom)) =< 2.
+
+terminate(_Reason, {Atoms, NFs}) ->
+    ets:delete(NFs),
+    ets:delete(Atoms).
+
